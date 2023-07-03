a65ca690e7fd5a2fa81c7dae9587cabc16c1b1cb
bell250 <ishimwebernardin90@gmail.com>
2023-07-03 17:14:24 +0200

    computerChoice function created

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-let a = prompt("eneter choice");
-//
+function getComputerChoice(){
+    let choices=["paper","rock","sissor"];
+    let selectChoice = Math.floor(Math.random()*choices.length);
+    let computerChoice =  choices[selectChoice];
+    console.log(computerChoice);
+    return computerChoice;
+}
+getComputerChoice();
