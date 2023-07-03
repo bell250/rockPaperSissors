de298a302b2e5540de6d14f6c5c5bdf70c56549f
bell250 <ishimwebernardin90@gmail.com>
2023-07-03 18:40:03 +0200

    function that play five rounds added

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ function getComputerChoice(){
     let choices=["paper","rock","sissor"];
     let selectChoice = Math.floor(Math.random()*choices.length);
     let computerChoice =  choices[selectChoice];
-    console.log(computerChoice);
     return computerChoice;
 }
-getComputerChoice();
+function game(playerSelection , computerSelection){
+    let computerScore = 0;
+    let playerScore = 0;
+    let round = 0;   
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+      }
+      if (
+        (playerSelection === 'rock' && computerSelection === 'sissor') ||
+        (playerSelection === 'sissor' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+      ) {
+        playerScore++;
+        roundWinner = 'you Won this round! '+playerSelection +'  beats '+computerSelection;
+      }
+      else if (
+        (computerSelection === 'rock' && playerSelection === 'sissor') ||
+        (computerSelection === 'sissor' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')
+      ) {
+        computerScore++;
+        roundWinner = 'you lose this round! '+ computerSelection +'  beats '+playerSelection;
+      }
+      return roundWinner;        
+}
+for(let i = 0 ; i <5 ; i++){
+let computerSelection = getComputerChoice();
+let playerSelection = prompt("Enter your Weapon");
+console.log(game(playerSelection , computerSelection));
+}
