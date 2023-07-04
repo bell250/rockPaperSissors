5349059eb57db3ebbe2bdb6b160469707eeb393e
bell250 <ishimwebernardin90@gmail.com>
2023-07-04 10:06:39 +0200

    now all functions are working propely.

--- script.js
+++ script.js
@@ -29,20 +29,20 @@ let playerScore = 0;
 let computerScore = 0;
 for(let i = 0 ; i < 5 ; i++){
 let computerSelection = getComputerChoice();
-let user = prompt("Enter your Weapon");
+let user = prompt("Enter your Weapon : rock , paper or sissor");
 let playerSelection =user.toLowerCase();
 let result = playRound(playerSelection , computerSelection);
 console.log(result);
-if (result.startsWith("you Win!")){
+if (result.startsWith("you Win! ")){
     playerScore++;
 }
-else if (result.startsWith("you lose!")){
+else if (result.startsWith("you lose! ")){
     computerScore++;
 }}
 if (playerScore > computerScore){
 console.log("Conglatulations you win this game "+computerScore+" "+playerScore);
 }
-else if(playerScore > computerScore){
+else if(playerScore < computerScore){
 console.log("you lose this game "+computerScore+" "+playerScore);}
 else{
     console.log("Draw game "+computerScore+" "+playerScore)
